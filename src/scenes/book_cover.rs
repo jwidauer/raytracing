51d717ebcb9aff2b968f7ca7306ba184ed825eb4
Jakob Widauer <jakob.widauer@gmail.com>
2023-05-01 02:35:29 +0200

    chore: disable fat lto, for faster compilation speed
    refactor: use seeded rng for book cover generation

--- src/scenes/book_cover.rs
+++ src/scenes/book_cover.rs
@@ -1,5 +1,4 @@
 use crate::objects::{BvhNode, Object};
-use crate::rand_ext::rand;
 use crate::textures::Checker;
 use crate::time::Time;
 use crate::{
@@ -8,6 +7,8 @@ use crate::{
     objects::{ObjectList, Sphere},
     vec3::Point3,
 };
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
 
 pub fn new(time: Time) -> impl Object {
     let mut world = ObjectList::new(vec![]);
@@ -18,26 +19,31 @@ pub fn new(time: Time) -> impl Object {
     world.add(ground);
 
     let mut random_spheres = ObjectList::new(vec![]);
+    let mut rng = ChaCha8Rng::seed_from_u64(123);
     for a in -11..11 {
         for b in -11..11 {
-            let choose_mat: f32 = rand::random();
+            let choose_mat: f32 = rng.gen();
             let center = Point3::new(
-                a as f32 + 0.9 * rand::random::<f32>(),
+                a as f32 + 0.9 * rng.gen::<f32>(),
                 0.2,
-                b as f32 + 0.9 * rand::random::<f32>(),
+                b as f32 + 0.9 * rng.gen::<f32>(),
             );
 
             if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
                 let sphere = if choose_mat < 0.8 {
                     // diffuse
-                    let albedo = Color::random() * Color::random();
-                    let center2 =
-                        center + Point3::new(0.0, rand::random_range::<f32>(0., 0.2), 0.0);
+                    let (r1, g1, b1, r2, g2, b2) = rng.gen();
+                    let albedo = Color::new(r1, g1, b1) * Color::new(r2, g2, b2);
+                    let center2 = center + Point3::new(0.0, rng.gen_range(0.0f32..0.2), 0.0);
                     Sphere::new_moving(center, center2, time, 0.2, Lambertian::new(albedo))
                 } else if choose_mat < 0.95 {
                     // metal
-                    let albedo = Color::random_range(0.5, 1.0);
-                    let fuzz = 0.5 * rand::random::<f32>();
+                    let albedo = Color::new(
+                        rng.gen_range(0.5f32..1.0),
+                        rng.gen_range(0.5f32..1.0),
+                        rng.gen_range(0.5f32..1.0),
+                    );
+                    let fuzz = 0.5 * rng.gen::<f32>();
                     Sphere::new(center, 0.2, Metal::new(albedo, fuzz))
                 } else {
                     // glass
